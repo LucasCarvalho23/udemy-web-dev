30ccf9fde6e5ba6923c5acc2f8d3ad1da44eae2d
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2023-11-20 19:59:23 -0300

    Update script.js

--- Curso Desenvolvimento Web - 20 cursos e 20 projetos/006. JavaScript/009. Ajax/004. XML/script.js
+++ Curso Desenvolvimento Web - 20 cursos e 20 projetos/006. JavaScript/009. Ajax/004. XML/script.js
@@ -18,15 +18,56 @@ class Movie {
         this.xmlFiles.onreadystatechange = () => {
             if (this.xmlFiles.readyState == 4 && this.xmlFiles.status == 200) {
                 this.XMLMovie = this.xmlFiles.responseText
-                console.log (this.XMLMovie)
                 this.parser = new DOMParser()
                 this.domFilms = this.parser.parseFromString(this.XMLMovie, 'text/xml')
        
                 this.jsonMovies = xmlToJson(this.domFilms)
 
-                console.log (this.domFilms)
-                console.log ('')
-                console.log (this.jsonMovies)
+                for (this.count in this.jsonMovies['filmes']['filme']) {
+
+
+                    this.item = this.jsonMovies['filmes']['filme'][this.count]
+
+                    this.divRow = document.createElement('div')
+                    this.divRow.className = 'row'
+
+                    this.divCol = document.createElement('div')
+                    this.divCol.className = 'col'
+
+                    this.p1 = document.createElement('p')
+                    this.p1.innerHTML = `<strong>Title: </strong> ${this.item['titulo']['#text']}`
+
+                    this.p2 = document.createElement('p')
+                    this.p2.innerHTML = `<strong>Resume: </strong> ${this.item['resumo']['#text']}`
+
+                    this.gender = ''
+
+                    for (this.g in this.item.genero) {
+                        console.log (this.item.genero[this.g])
+                    }
+
+                    this.p3 = document.createElement('p')
+                    this.p3.innerHTML = `<strong>Gender: </strong> ${this.gender}`
+
+                    this.p4 = document.createElement('p')
+                    this.p4.innerHTML = `<strong>Casting: </strong> ${this.item['titulo']['#text']}`
+
+                    this.p5 = document.createElement('p')
+                    this.p5.innerHTML = `<strong>Date: </strong> ${this.item['titulo']['#text']}`
+                    
+                    this.hr = document.createElement('hr')
+
+                    this.divRow.appendChild(this.divCol)
+                    this.divCol.appendChild(this.p1)
+                    this.divCol.appendChild(this.p2)
+                    this.divCol.appendChild(this.p3)
+                    this.divCol.appendChild(this.p4)
+                    this.divCol.appendChild(this.p5)
+                    this.divCol.appendChild(this.hr)
+
+                    document.querySelector("#container-list").appendChild(this.divRow)
+                }
+
 
                 /* 
 
@@ -39,7 +80,7 @@ class Movie {
                         <p><strong>Date: </strong> Date for movie</p>
                     </div>
                 </div>
-                
+
                 */
             }
 
