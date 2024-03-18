a70dbcdd2eaddcc9e7010d27e49bd9676edd8046
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-03-17 22:45:45 -0300

    Selectors
    Selectors

---
+++ Curso Desenvolvimento Web - 20 cursos e 20 projetos/010. JQuery/001. Selectors/javascript/script.js
@@ -0,0 +1,28 @@
+/* 
+
+    Selecting elements by tags: $(tag)
+    Selecting elements by class: $(.class)
+    Selecting elements by id: $(#id)
+
+    Obs.: $ -> allows access to the JQuery library
+
+*/
+
+console.log($('h1'))
+console.log($('div'))
+console.log($('ul'))
+console.log($('input'))
+console.log($('img'))
+
+console.log($('.itemMenu2'))
+
+console.log($('#img-id'))
+
+console.log($('li.itemMenu2'))
+
+console.log($('.list1 .example1'))
+
+console.log($('.list1 .example1:first'))
+console.log($('.list1 .example1:last'))
+console.log($('.list1 .example1:even'))
+console.log($('.list1 .example1:odd')) 
