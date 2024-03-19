038c9a036956fbf78618615789eda8c097827182
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-03-18 22:31:08 -0300

    html(), text() and val()
    html(), text() and val()

---
+++ Curso Desenvolvimento Web - 20 cursos e 20 projetos/010. JQuery/004. html(), text() and val()/javascript/script.js
@@ -0,0 +1,12 @@
+console.log('Content DIV01:', $('#div01').html())
+console.log('Content DIV02:', $('#div02').html())
+console.log('Content DIV01:', $('#div01').html("I'm the new value"))
+
+console.log('Content DIV01:', $('#div01').html("<strong>I'm the other value</strong>"))
+console.log('Content DIV01:', $('#div01').text("<strong>I'm the other value</strong>"))
+
+console.log($('#name-input-id').val())
+console.log($('#origin-select-id').val())
+
+console.log($('#name-input-id').val('Oak'))
+console.log($('#origin-select-id').val('3'))
