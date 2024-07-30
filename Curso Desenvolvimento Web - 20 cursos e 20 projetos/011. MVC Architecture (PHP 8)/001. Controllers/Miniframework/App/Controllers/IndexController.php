f6fa281a5f35d187137cadf4cc020587b268c3bc
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-07-30 17:47:47 -0300

    MVC Architecture
    MVC Architecture

---
+++ Curso Desenvolvimento Web - 20 cursos e 20 projetos/011. MVC Architecture (PHP 8)/001. Controllers/Miniframework/App/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Controllers;
+
+    class IndexController {
+
+        public function index() {
+            echo "Chegamos na index";
+        }
+
+        public function about_us() {
+            echo "Chegamos ao about us";
+        }
+
+    }
+
+?>
